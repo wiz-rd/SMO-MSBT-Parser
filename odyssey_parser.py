0f806ec6883596a51a42db7c8d26e1181fc78a45
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2024-07-26 19:46:44 -0400

    Fixed regex and added flatten method

--- odyssey_parser.py
+++ odyssey_parser.py
@@ -6,7 +6,7 @@ import re
 # = Setting const and global variables
 # ================================
 
-SP_CHAR = dict()
+SP_CHAR: dict = {}
 
 # ================================
 # = Setting variables and things
@@ -24,6 +24,55 @@ def insert_str(item, string: str, index: int):
     return string[index:] + item + string[:index]
 
 
+def flatten():
+    """
+    A function to "flatten" the special characters and their indices.
+    Essentially, combines characters that should be combined together.
+    """
+    # start from the end of the list so that when you combine the lists,
+    # they are combined from first to last instead of the other way around
+    previous_index = None
+    # TODO: DELETE DEBUGGING
+    print(SP_CHAR)
+
+    for string_index in sorted(SP_CHAR, reverse=True):
+        # if an adjacent index is found, merge the two downwards
+        # otherwise just continue.
+        if previous_index is None: previous_index = string_index
+
+        # if two indexes are one apart
+        # e.g. if it's "@@<null>", then don't put their
+        # indices as "1, 2, 3". Put them as just 1
+        # with the list attached to the key of 1 as
+        # ["@", "@", "<null>"]
+
+        if (string_index - 1) in SP_CHAR:
+            SP_CHAR[string_index - 1] += SP_CHAR.pop(string_index)
+
+        # this is kind of hard to explain
+        # essentially, this performs a similar role as the if statement above
+        # but it calculates the length of "<null>" and whatnot - that way
+        # two "<null><null>"s can be appended together, for example.
+        # in the previous if statement, they would not be appended together,
+        # because their indices would be 0 and 6 or so, and: 0 - 1 != 6
+        # so even though they, visually, are against each other, to the code they are not.
+        # this seeks to fix that.
+
+        distance_between_indices = previous_index - string_index if previous_index is not None else -100
+        length_of_previous_word = len("".join(SP_CHAR[string_index])) if string_index in SP_CHAR else -100
+        short_enough = distance_between_indices == length_of_previous_word if previous_index in SP_CHAR else False
+
+        # ensure they aren't the same index AND if the last index minus this index is less than or equal to the length of the characters stored,
+        # then combine the two indices
+        try:
+            if (previous_index != string_index) and short_enough:
+                SP_CHAR[string_index] += SP_CHAR.pop(previous_index)
+        except KeyError:
+            print("Skipping flattening adjacent nulls for now.")
+
+        previous_index = string_index
+
+
 def cleanup():
     """
     Uses Regex to remove all non-alphanumeric
@@ -36,7 +85,7 @@ def cleanup():
     # filter out everything that is either the phrase
     # <null> OR is NOT an (English) alphanumeric
     # character. Punctuation and whitespace are allowed
-    regex_filter_out = re.compile("(?<![\w<])<null>(?![\w>])|[^,\s\.\!?a-zA-Z0-9]")
+    regex_filter_out = re.compile("(?<![<])<null>(?![>])|[^:;,\s\.\!?a-zA-Z0-9]")
 
     # getting input
     original_text = txt_in.get("1.0", "end-1c")
@@ -51,7 +100,7 @@ def cleanup():
     # instead of just a singular index. This way
     # you can replace every occurrence of the
     # string in question
-    
+
     # TODO: fix it so that when the output string has been parsed and is shorter,
     # inserting back the original characters doesn't break anything.
     # right now, it breaks stuff becauss it tries
@@ -65,25 +114,64 @@ def cleanup():
             offset = 1
         else:
             offset = 0
-        
+
         index_in_original_string = original_text.index(item, index_in_original_string + offset)
 
         # store the index of each special character
         # so we can add it back to the string later
         # after translation / modification
 
-        # if there isn't a value yet, default to an empty list'
-        if item not in SP_CHAR:
-            SP_CHAR[item] = list()
+        # if no character has been found at the given index yet,
+        # create a new list for characters found there to be added to
+        if index_in_original_string not in SP_CHAR:
+            SP_CHAR[index_in_original_string] = [item]
+        else:
+            # append each item found to the end of the index's list
+            SP_CHAR[index_in_original_string] = SP_CHAR[index_in_original_string].append(item)
 
-        # if the item already exists, append it to the current list
-        SP_CHAR[item].append(index_in_original_string)
+        # append a list of the location and the
+        # item itself into the overall list
+        # SP_CHAR.append([index_in_original_string, item])
 
     output_text = regex_filter_out.sub("", original_text)
 
-    # SP_CHAR = special_chars
+    # combining adjacent entries in the dictionary
+    flatten()
+    flatten()
     txt_out.insert("1.0", output_text)
-    print(found)
+    print(SP_CHAR)
+
+
+def working_revert():
+    """
+    Uses a dictionary stored to restore every character removed from
+    the input to their proper locations
+    """
+    # sort the index of each item
+    # doing it backwards because it causes issues if you do it forwards
+    indexes_found = sorted(SP_CHAR.keys(), reverse=True)
+
+    input_string = txt_in.get("1.0", "end-1c")
+    output_string = txt_out.get("1.0", "end-1c")
+
+    # the ratio of output to input lengths- essentially, will
+    # be used for placement of special characters instead of
+    # *just* the index
+    ratio = len(output_string) / len(input_string)
+
+    # for each character removed
+    for index in indexes_found:
+        # get the item by joining all
+        # characters found at that index
+        item = "".join(SP_CHAR[index])
+        normalized_location = round(index * ratio)
+        # make sure it doesn't exceed the length of the output string
+        normalized_location -= 1 if normalized_location >= len(output_string) else 0
+
+        print(f"Inserting string {item} at location {normalized_location}")
+        output_string = insert_str(item, output_string, normalized_location)
+
+    txt_in.insert("1.0", output_string + "\n\n")
 
 
 def revert():
@@ -91,12 +179,32 @@ def revert():
     Uses a dictionary stored in a variable to restore every character removed
     from the input to their proper locations.
     """
+    # sort by the first item in each list (so the index
+    # of the original string, not the item itself)
+    # (ensures they'll be inserted in the correct order)
+    SP_CHAR.sort(key=lambda x: x[0], reverse=True)
+
     # getting this string manually because Tkinter
     # text box's grid like interface is kind of ridiculous
+    input_string = txt_in.get("1.0", "end-1c")
     output_string = txt_out.get("1.0", "end-1c")
-    for ch in SP_CHAR:
-        for location in ch:
-            output_string = insert_str(ch, output_string, location)
+
+    # the ratio of output to input lengths- essentially, will
+    # be used for placement of special characters instead of
+    # *just* the index
+    ratio = len(output_string) / len(input_string)
+
+    # for each character removed
+    for group in SP_CHAR:
+        print(group)
+        # get the index
+        location, item = group
+        normalized_location = round(location * ratio)
+        # make sure it doesn't exceed the length of the output string
+        normalized_location -= 1 if normalized_location >= len(output_string) else 0
+
+        print(f"Inserting string {item} at location {normalized_location}")
+        output_string = insert_str(item, output_string, normalized_location)
 
     txt_in.insert("1.0", output_string + "\n\n")
 
@@ -126,7 +234,7 @@ btn_revert = tk.Button(
     text="Revert text",
     width=12,
     height=2,
-    command=revert,
+    command=working_revert,
 )
 
 txt_in = tk.Text(frame_i, width=50)
